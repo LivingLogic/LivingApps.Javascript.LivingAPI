ac3161d2679d9c22716d07bf307903fa017b8dfd
René Schwarzinger <rene.schwarzinger@milleniumfrog.de>
2018-05-30 13:53:14 +0200

    set config/tsconfig.umd.json as default tsconfig.json

--- shellscripts/build.sh
+++ shellscripts/build.sh
@@ -1,7 +1,7 @@
 rm -Rf ./dist
 echo build LivingApps.Javascript.LivingAPI
 tsc -p ./config/tsconfig.es2015.json & 
-tsc -p ./config/tsconfig.umd.json
+tsc -p ./tsconfig.json
 wait
 echo copy modules
 cp -R ./src/modules ./dist/es2015/
