02cfc0e2e57eb79ad8173367d03b2ca14ea07b9e
René Schwarzinger <rene.schwarzinger@milleniumfrog.de>
2018-06-08 12:05:46 +0200

    .get() - testsuite added - created 5 templates, saved names in lsdktemplates enum - .get() testsuite description

--- src/livingsdk.test.ts
+++ src/livingsdk.test.ts
@@ -3,12 +3,20 @@ import { expect } from 'chai';
 import 'mocha';
 import {livingappsData as lsd } from './config'
 
+enum lsdktemplates {
+	default= 'default',
+	loggedIn = 'loggedInUsers',
+	admin = 'withAdminPriveleges',
+	permissions = 'withPermissions',
+	workpriv = 'withWorkingPriveleges'
+}
+
 describe('LivingSDK: ', () => {
 	/*
 	 * test login
 	 * - getAuthToken
 	 */
-	describe('login', () => {
+	describe('.login()', () => {
 		it('no login', () => {
 			let lsdk = new LivingSDK({loginRequired: false});
 			return lsdk.login().then((auth_token: Auth_Token) => {
@@ -28,7 +36,7 @@ describe('LivingSDK: ', () => {
 				(<any>lsdk).session = Promise.resolve("10");
 				return (<any>lsdk).session;
 			}).then(() => {
-				return lsdk.get(lsd.appId, 'closed');
+				return lsdk.get(lsd.appId, lsdktemplates.loggedIn);
 			})
 			.then(() => {
 				throw new Error("user still logged in");
@@ -41,7 +49,89 @@ describe('LivingSDK: ', () => {
 			return lsdk.login().then((auth_token: Auth_Token) => {
 				// teste ob ergebnis leer ist
 				expect(auth_token).to.equal(undefined);
+			});
+		});
+	});
+
+
+	describe('.get()', () => {
+		// not logged in user
+		function createMinLSDK () {
+			return new LivingSDK({loginRequired: false});
+		}
+		// logged in user
+		function createMaxLSDK () {
+			return new LivingSDK({}, lsd.username, lsd.password);
+		}
+		describe('permissions without login', () => {
+			it('request default', () => {
+				return createMinLSDK().get(lsd.appId);
+			});
+			it('request loggedInUsers', () => {
+				return createMinLSDK().get(lsd.appId, lsdktemplates.loggedIn)
+					.then(() => {
+						throw new Error('should not reach this part of code');
+					})
+					.catch((err: any) => {
+						expect(err.message).to.equal('Request failed with status code 403');
+					})
 			})
-		})
+			it('request withPermissionsForApp', () => {
+				return createMinLSDK().get(lsd.appId, lsdktemplates.permissions)
+					.then(() => {
+						throw new Error('should not reach this part of code');
+					})
+					.catch((err: any) => {
+						expect(err.message).to.equal('Request failed with status code 403');
+					})
+			})
+			it('request withWorkingPrivelegesApp', () => {
+				return createMinLSDK().get(lsd.appId, lsdktemplates.workpriv)
+					.then(() => {
+						throw new Error('should not reach this part of code');
+					})
+					.catch((err: any) => {
+						expect(err.message).to.equal('Request failed with status code 403');
+					})
+			})
+			it('request withAdminPriveleges', () => {
+				return createMinLSDK().get(lsd.appId, lsdktemplates.admin)
+					.then(() => {
+						throw new Error('should not reach this part of code');
+					})
+					.catch((err: any) => {
+						expect(err.message).to.equal('Request failed with status code 403');
+					})
+			})
+		});
+
+		describe('permissions with admin login', () => {
+			it('request default', () => {
+				return createMaxLSDK().get(lsd.appId);
+			});
+			it('request loggedInUsers', () => {
+				return createMaxLSDK().get(lsd.appId, lsdktemplates.loggedIn);
+			})
+			it('request withPermissionsForApp', () => {
+				return createMaxLSDK().get(lsd.appId, lsdktemplates.permissions);
+			})
+			it('request withWorkingPrivelegesApp', () => {
+				return createMaxLSDK().get(lsd.appId, lsdktemplates.workpriv);
+			})
+			it('request withAdminPriveleges', () => {
+				return createMaxLSDK().get(lsd.appId, lsdktemplates.admin);
+			})
+		});
+
+		it('request not existing app', () => {
+			return createMaxLSDK().get('afsdadstasda')
+				.then(() => {
+					throw new Error('should not reach this part of code');
+				})
+				.catch((err: any) => {
+					expect(err.message).to.equal('Request failed with status code 404');
+				})
+		});
 	})
+
 });
